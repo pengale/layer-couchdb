36f34e189dc89e58f2dd24d036af98cfff85fb35
Pete Vander Giessen <petevg@petevg.com>
2016-04-11 10:36:35 -0400

    First functional version of a layered couchdb charm.
    I still have many TODOs. The Charm needs to tell connecting services
    about admin credentials, and I have a lot of misc TODOs scattered
    throughout the file.
    
    But the Charm stands up a server with a functional version of couch. I
    was able to connect to it, login, and create a database and
    documents. This juju stuff is pretty cool. :-)

--- reactive/couchdb.py
+++ reactive/couchdb.py
@@ -1,8 +1,84 @@
+import codecs
+import shutil
 import subprocess
+import configparser
+from shutil import copyfile
 
-from charmhelpers.core.hookenv import open_port, config,
+from charmhelpers.core.hookenv import open_port, config
 from charmhelpers.fetch import apt_install, add_source, apt_update
-from charms.reactive import hook
+from charms.reactive import hook, when
+
+
+def _set_couch_config(config_path="/etc/couchdb"):
+    """
+    Edit the default couch config files.
+
+    @param string config_path: The full path to the directory where the files are stored in
+                               the system. Most of the time, the default will be fine.
+
+    """
+
+    # Generate values to slot into couch config files
+    ADMIN_PASS = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
+    REPL_PASS = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
+    # TODO: Is there a more "Charming" way of generating passwords?
+    COUCH_CONFIGS = [
+        { "name": "local", "sections": [
+            { "name": "httpd", "values": [
+                { "key": "bind_address", "value": config("couchdb-bind-addr") },
+                { "key": "port", "value": str(config("couchdb-port")) },
+            ]},
+            { "name": "couch_httpd_auth", "values": [
+                { "key": "require_valid_user", "value": "true" }
+            ]},
+            { "name": "admins", "values": [
+                { "key": "admin", "value": ADMIN_PASS },
+                { "key": "replication", "value": REPL_PASS },
+            ]}
+        ]},
+        { "name": "default", "sections": [
+            { "name": "httpd", "values": [
+                { "key": "bind_address", "value": config("couchdb-bind-addr") },
+            ]}
+        ]},
+        { "name": "juju_generated", "sections": [
+            {"name": "credentials" , "values": [
+                { "key": "admin_pass", "value": ADMIN_PASS },
+                { "key": "repl_pass", "value": REPL_PASS },
+            ]}
+        ]}
+    ]
+
+    # Write out the config we've generated above.
+    # TODO: tell configparser to stop clobbering the comments in the
+    # original local.ini and default.ini files, if possible.
+    for conf in COUCH_CONFIGS:
+        parser = configparser.ConfigParser()
+        file_path = "{}/{}.ini".format(config_path, conf['name'])
+        try:
+            parser.read_file(codecs.open(file_path, "r", "utf8"))
+            shutil.copyfile(file_path, "{}.bak".format(file_path))  # Backups are good.
+        except(FileNotFoundError):
+            # If the file doesn't exist, that's okay. We'll create it below.
+            pass
+        for section in conf['sections']:
+            if not parser.has_section(section['name']):
+                parser.add_section(section['name'])
+
+            for value in section['values']:
+                parser.set(section['name'], value["key"], value["value"])
+                # TODO: It would be nicer to flatten the config tree
+                # we generate above, so that we can avoid triple
+                # nesting things here. (Performance isn't really a
+                # concern for something this small, but it rubs me the
+                # wrong way.)
+
+        with open(file_path, "w") as conf_file:
+            parser.write(conf_file)
+
+    # TODO: change the permissions on the juju_generated file, so that
+    # only root can read it.
+    # TODO: ... or better yet, store it in the leadership settings (see Postgresql charm)
 
 
 @hook("install")
@@ -15,52 +91,34 @@ def install():
     here.
 
     """
+    # TODO: audit these packages. I don't think that all of them are necessary.
+    # Install packages
     apt_install(['python-software-properties', 'debconf', 'debconf-utils'])
-    add_source("ppa:facter-plugins/ppa")
     apt_update()
-    apt_install(['couchdb', 'facter', 'facter-customfacts-plugin', 'uuid', 'python-couchdb',
-                 'pwgen'])
+    apt_install(['couchdb', 'uuid', 'python-couchdb', 'pwgen'])
 
-    # TODO: is this actually the most Charming way to do it?
-    DEFAULT_ADMIN_PASSWORD=subprocess.check_output(['pwgen', '-N1']).strip()
-    DEFAULT_REPLICATION_PASSWORD=subprocess.check_output(['pwgen', '-N1']).strip()
-    
-    """
-    TODO: convert this facter stuff to some calls to the Python
-    ConfigParser, and cut out the facter install stuff above.
+    # Edit config files
+    _set_couch_config()
 
-    fact-add couchdb_hostname `facter fqdn`
-    fact-add couchdb_ip `facter ipaddress`
-    fact-add couchdb_port 5984
-    fact-add couchdb_replication ${DEFAULT_REPLICATION_PASSWORD}
-    fact-add couchdb_admin ${DEFAULT_ADMIN_PASSWORD}
-
-    sed -i.bak -e "s/bind_address =.*/bind_address = `facter couchdb_ip`/" /etc/couchdb/default.ini
-    sed -i.bak -e "s/;port =.*/port = `facter couchdb_port`/" \
-               -e "s/;bind_address.*/bind_address = `facter couchdb_ip`/" \
-               -e "s/; require_valid_user = false/require_valid_user = true/" \
-               -e "s/;admin =.*/admin = ${DEFAULT_ADMIN_PASSWORD}/" \
-               /etc/couchdb/local.ini
-    echo "replication = ${DEFAULT_REPLICATION_PASSWORD}" >> /etc/couchdb/local.ini
-    """
-
-    # TODO: maybe make an @when handler that starts when installed,
-    # then make a handler that opens port once when both installed and
-    # started?
+    # Start couch
+    # TODO: Move this to a @when handler? (or just add an @when to start)
     start()
 
-    # TOOD open-port `facter couchdb_port`/TCP
-    # TODO open_port(<put port here>)
+    # Open the couch port
+    # TODO: Move this to @when handler?
+    open_port(config("couchdb-port"))
+
 
 @hook("start")
 def start():
     """
     Start couch, or, in the case where couch is already running, restart couch.
     """
-    if subprocess.call(['service', 'couchdb', 'status']):
+    if not subprocess.call(['service', 'couchdb', 'status']):
         subprocess.check_call(['service', 'couchdb', 'restart'])
     else:
         subprocess.check_call(['service', 'couchdb', 'start'])
+
 
 @hook("stop")
 def stop():
@@ -69,13 +127,21 @@ def stop():
     """
     subprocess.check_call(['service', 'couchdb', 'stop'])
 
+
 @hook("db-relation-joined")
-def db_relation_join():
+def db_relation_joined():
     """
-    TODO: read up about relations work in the reactive work, and write
-    appropriate code here.
-
+    # TODO: Actually convert this into Python.
+    # TODO: figure out how to get the right values for couchdb-host and couchdb-ip.
     """
-    # TODO: relation-set host=`facter couchdb_hostname` ip=`facter couchdb_ip`  port=`facter couchdb_port` admin=`facter couchdb_admin`
-    # TODO: echo $ENSEMBLE_REMOTE_UNIT joined
-
+    subprocess.check_call([
+        'relation-set',
+        "host={}".format(config("couchdb-host")),
+        "ip={}".format(config("couchdb-ip")),
+        "port={}".format(config("couchdb-port"))
+    ])
+    subprocess.check_call([
+        "echo",
+        "$ENSEMBLE_REMOTE_UNIT",
+        "joined"
+    ])
