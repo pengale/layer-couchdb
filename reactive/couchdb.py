28620276e60570f68bc18d566c78c07ed9c92aa2
Pete Vander Giessen <petevg@petevg.com>
2016-04-11 17:01:59 -0400

    Style cleanup.
    Replaced a lot of double quotes with single quotes, and made other
    readability improvements.

--- reactive/couchdb.py
+++ reactive/couchdb.py
@@ -9,7 +9,7 @@ from charmhelpers.fetch import apt_install, add_source, apt_update
 from charms.reactive import hook, when
 
 
-def _set_couch_config(config_path="/etc/couchdb"):
+def _set_couch_config(config_path='/etc/couchdb'):
     """
     Edit the default couch config files.
 
@@ -23,19 +23,19 @@ def _set_couch_config(config_path="/etc/couchdb"):
     REPL_PASS = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
     # TODO: Is there a more "Charming" way of generating passwords?
     COUCH_CONFIGS = [
-        { "name": "local", "entries": [
-            { "section": "httpd", "key": "bind_address", "value": config("couchdb-bind-addr") },
-            { "section": "httpd", "key": "port", "value": str(config("couchdb-port")) },
-            { "section": "couch_httpd_auth", "key": "require_valid_user", "value": "true" },
-            { "section": "admins", "key": "admin", "value": ADMIN_PASS },
-            { "section": "admins", "key": "replication", "value": REPL_PASS },
+        { 'name': 'local', 'entries': [
+            { 'section': 'httpd', 'key': 'bind_address', 'value': config('couchdb-bind-addr') },
+            { 'section': 'httpd', 'key': 'port', 'value': str(config('couchdb-port')) },
+            { 'section': 'couch_httpd_auth', 'key': 'require_valid_user', 'value': 'true' },
+            { 'section': 'admins', 'key': 'admin', 'value': ADMIN_PASS },
+            { 'section': 'admins', 'key': 'replication', 'value': REPL_PASS },
         ]},
-        { "name": "default", "entries": [
-            { "section": "httpd", "key": "bind_address", "value": config("couchdb-bind-addr") },
+        { 'name': 'default', 'entries': [
+            { 'section': 'httpd', 'key': 'bind_address', 'value': config('couchdb-bind-addr') },
         ]},
-        { "name": "juju_generated", "entries": [
-            { "section": "creds", "key": "admin_pass", "value": ADMIN_PASS },
-            { "section": "creds", "key": "repl_pass", "value": REPL_PASS },
+        { 'name': 'juju_generated', 'entries': [
+            { 'section': 'creds', 'key': 'admin_pass', 'value': ADMIN_PASS },
+            { 'section': 'creds', 'key': 'repl_pass', 'value': REPL_PASS },
         ]}
     ]
 
@@ -44,21 +44,21 @@ def _set_couch_config(config_path="/etc/couchdb"):
     # original local.ini and default.ini files.
     for conf in COUCH_CONFIGS:
         parser = configparser.ConfigParser()
-        file_path = "{}/{}.ini".format(config_path, conf['name'])
+        file_path = '{}/{}.ini'.format(config_path, conf['name'])
         try:
-            parser.read_file(codecs.open(file_path, "r", "utf8"))
-            shutil.copyfile(file_path, "{}.bak".format(file_path))  # Backups are good.
+            parser.read_file(codecs.open(file_path, 'r', 'utf8'))
+            shutil.copyfile(file_path, '{}.bak'.format(file_path))  # Backups are good.
         except(FileNotFoundError):
             # If the file doesn't exist, that's okay. We'll create it below.
             # TODO: we should probably raise if default.ini doesn't exist.
             pass
-        for entry in conf["entries"]:
-            if not parser.has_section(entry["section"]):
-                parser.add_section(entry["section"])
+        for entry in conf['entries']:
+            if not parser.has_section(entry['section']):
+                parser.add_section(entry['section'])
 
-            parser.set(entry["section"], entry["key"], entry["value"])
+            parser.set(entry['section'], entry['key'], entry['value'])
 
-        with open(file_path, "w") as conf_file:
+        with open(file_path, 'w') as conf_file:
             parser.write(conf_file)
 
     # TODO: change the permissions on the juju_generated file, so that
@@ -66,7 +66,7 @@ def _set_couch_config(config_path="/etc/couchdb"):
     # TODO: ... or better yet, store it in the leadership settings (see Postgresql charm)
 
 
-@hook("install")
+@hook('install')
 def install():
     """
     Install current version of couchdb from package manager.
@@ -78,9 +78,9 @@ def install():
     """
     # TODO: audit these packages. I don't think that all of them are necessary.
     # Install packages
-    apt_install(['python-software-properties', 'debconf', 'debconf-utils'])
-    apt_update()
-    apt_install(['couchdb', 'uuid', 'python-couchdb', 'pwgen'])
+    # TODO: also, read up on the apt layer, and determine whether the below can be simplified.
+    apt_install(['python-software-properties', 'debconf', 'debconf-utils', 'couchdb',
+                 'uuid', 'python-couchdb', 'pwgen'])
 
     # Edit config files
     _set_couch_config()
@@ -91,10 +91,10 @@ def install():
 
     # Open the couch port
     # TODO: Move this to @when handler?
-    open_port(config("couchdb-port"))
+    open_port(config('couchdb-port'))
 
 
-@hook("start")
+@hook('start')
 def start():
     """
     Start couch, or, in the case where couch is already running, restart couch.
@@ -105,7 +105,7 @@ def start():
         subprocess.check_call(['service', 'couchdb', 'start'])
 
 
-@hook("stop")
+@hook('stop')
 def stop():
     """
     Halt couch.
@@ -113,7 +113,7 @@ def stop():
     subprocess.check_call(['service', 'couchdb', 'stop'])
 
 
-@hook("db-relation-joined")
+@hook('db-relation-joined')
 def db_relation_joined():
     """
     # TODO: Actually convert this into Python.
@@ -121,12 +121,12 @@ def db_relation_joined():
     """
     subprocess.check_call([
         'relation-set',
-        "host={}".format(config("couchdb-host")),
-        "ip={}".format(config("couchdb-ip")),
-        "port={}".format(config("couchdb-port"))
+        'host={}'.format(config('couchdb-host')),
+        'ip={}'.format(config('couchdb-ip')),
+        'port={}'.format(config('couchdb-port'))
     ])
     subprocess.check_call([
-        "echo",
-        "$ENSEMBLE_REMOTE_UNIT",
-        "joined"
+        'echo',
+        '$ENSEMBLE_REMOTE_UNIT',
+        'joined'
     ])
