ba45c237980b609f10ec3574fc80d3616a54d876
Pete Vander Giessen <petevg@petevg.com>
2016-04-11 20:19:33 -0400

    Cleaned up comments and naming conventions.

--- reactive/couchdb.py
+++ reactive/couchdb.py
@@ -1,96 +1,148 @@
-from shutil import copyfile
 import codecs
 import configparser
 import shutil
 import subprocess
+from shutil import copyfile
 
-from charmhelpers.core.hookenv import open_port, config
+from charmhelpers.core.hookenv import open_port, config, relation_set, relation_get
 from charmhelpers.fetch import apt_install, add_source, apt_update
-from charms.reactive import hook, when
+from charms.leadership import leader_set, leader_get
+from charms.reactive import hook, when, not_unless
 
+#
+# Helpers
+#
 
-def _set_couch_config(config_path='/etc/couchdb'):
+def _write_config(config_path, name, entries):
+    """
+    Write out a given config to a file.
+
+    @param str config_path: Directory containing configs.
+    @param str name: Name of the config file (leave out the .ini part)
+    @param list entries: A dict of entries to write to the config, in the following format:
+                         { "section": <section_name>, "key": <key name>, "value": <value> }
+                         (Note that all values in the dict must be strings.)
+
+    """
+    parser = configparser.ConfigParser()
+    file_path = '{}/{}.ini'.format(config_path, name)
+
+    # Backup the existing config.
+    shutil.copyfile(file_path, '{}.bak'.format(file_path))
+
+    # Read it.
+    with codecs.open(file_path, 'r', 'utf8') as conf_file:
+        parser.read_file(conf_file)
+
+    # Modify it.
+    for entry in entries:
+        if not parser.has_section(entry['section']):
+            parser.add_section(entry['section'])
+
+    parser.set(entry['section'], entry['key'], entry['value'])
+
+    # Write it out.
+    with open(file_path, 'w') as conf_file:
+        parser.write(conf_file)
+
+    # TODO: tell configparser to stop clobbering the comments in the
+    # original file.
+
+def _write_couch_configs(config_path='/etc/couchdb'):
     """
     Edit the default couch config files.
 
-    @param string config_path: The full path to the directory where the files are stored in
-                               the system. Most of the time, the default will be fine.
+    @param str config_path: The full path to the directory where the files are stored in
+                            the system. Most of the time, the default will be fine.
 
     """
 
-    # Generate values to slot into couch config files
-    ADMIN_PASS = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
-    REPL_PASS = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
-    # TODO: Is there a more "Charming" way of generating passwords?
     COUCH_CONFIGS = [
         { 'name': 'local', 'entries': [
             { 'section': 'httpd', 'key': 'bind_address', 'value': config('couchdb-bind-addr') },
             { 'section': 'httpd', 'key': 'port', 'value': str(config('couchdb-port')) },
-            { 'section': 'couch_httpd_auth', 'key': 'require_valid_user', 'value': 'true' },
-            { 'section': 'admins', 'key': 'admin', 'value': ADMIN_PASS },
-            { 'section': 'admins', 'key': 'replication', 'value': REPL_PASS },
         ]},
         { 'name': 'default', 'entries': [
             { 'section': 'httpd', 'key': 'bind_address', 'value': config('couchdb-bind-addr') },
         ]},
-        { 'name': 'juju_generated', 'entries': [
-            { 'section': 'creds', 'key': 'admin_pass', 'value': ADMIN_PASS },
-            { 'section': 'creds', 'key': 'repl_pass', 'value': REPL_PASS },
-        ]}
     ]
 
-    # Write out the config we've generated above.
-    # TODO: tell configparser to stop clobbering the comments in the
-    # original local.ini and default.ini files.
     for conf in COUCH_CONFIGS:
-        parser = configparser.ConfigParser()
-        file_path = '{}/{}.ini'.format(config_path, conf['name'])
-        try:
-            parser.read_file(codecs.open(file_path, 'r', 'utf8'))
-            shutil.copyfile(file_path, '{}.bak'.format(file_path))  # Backups are good.
-        except(FileNotFoundError):
-            # If the file doesn't exist, that's okay. We'll create it below.
-            # TODO: we should probably raise if default.ini doesn't exist.
-            pass
-        for entry in conf['entries']:
-            if not parser.has_section(entry['section']):
-                parser.add_section(entry['section'])
+        _write_config(config_path, conf['name'], conf['entries'])
 
-            parser.set(entry['section'], entry['key'], entry['value'])
+#
+# Handlers
+#
 
-        with open(file_path, 'w') as conf_file:
-            parser.write(conf_file)
+@when("leader-elected")
+def maybe_generate_passwords():
+    """
+    If we're the leader, and we haven't generated passwords yet, generate them.
 
-    # TODO: change the permissions on the juju_generated file, so that
-    # only root can read it.
-    # TODO: ... or better yet, store it in the leadership settings (see Postgresql charm)
+    @param string name: The name of the key that will store the password.
+
+    """
+    if not leader_get('passwords'):
+        admin_pass = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
+        repl_pass = subprocess.check_output(['pwgen', '-N1']).strip().decode('utf-8')
+        leader_set(passwords={"admin_pass": admin_pass, "repl_pass": repl_pass})
+
+
+@when("leader-settings-changed")
+def end_admin_party(config_path='/etc/couchdb'):
+    """
+    Couch starts out in "admin party" mode, which means that anyone
+    can create and edit databases. Once it looks like the leader has
+    generated some passwords, write them out to disk.
+
+    @param str config_path: The location of the config files in the system.
+
+    # TODO: this is not getting triggered. Not sure whether it's an
+    # issue with this hook, with the 'maybe_generate_passwords' hook
+    # above, or with my understanding of how the leadership automagic
+    # works in general.
+
+    """
+    passwords = leader_get('passwords')
+
+    if not passwords:
+        return
+
+    entries = [
+        { 'section': 'admins', 'key': 'admin', 'value': passwords['admin_pass'] },
+        { 'section': 'admins', 'key': 'replication', 'value': passwords['repl_pass'] },
+        { 'section': 'couch_httpd_auth', 'key': 'require_valid_user', 'value': 'true' },
+        # TODO: get rid of the following section? It's mainly for manual testing, and
+        # it does not fit in with couch's security model.
+        { 'section': 'juju_notes', 'key': 'admin_pass', 'value': passwords['admin_pass'] },
+        { 'section': 'juju_notes', 'key': 'repl_pass', 'value': passwords['repl_pass'] },
+    ]
+    _write_config(config_path, "local", entries)
+
+    start()  # TODO: trigger start with a @when hook
 
 
 @hook('install')
 def install():
     """
-    Install current version of couchdb from package manager.
+    Install the current version of couchdb from package manager.
 
-    # TODO: determine whether it is better to specify the distros for
-    which this charm works, or specify a specify version of couch
-    here.
+    Start it up once installed. Defer ending the admin party -- we'll
+    handle it elsewhere.
 
     """
-    # TODO: audit these packages. I don't think that all of them are necessary.
     # Install packages
-    # TODO: also, read up on the apt layer, and determine whether the below can be simplified.
+    # TODO: audit these packages. I don't think that all of them are necessary.
     apt_install(['python-software-properties', 'debconf', 'debconf-utils', 'couchdb',
                  'uuid', 'python-couchdb', 'pwgen'])
 
     # Edit config files
-    _set_couch_config()
+    _write_couch_configs()
 
     # Start couch
-    # TODO: Move this to a @when handler? (or just add an @when to start)
-    start()
+    start()  # TODO: trigger with an @when hook
 
     # Open the couch port
-    # TODO: Move this to @when handler?
     open_port(config('couchdb-port'))
 
 
@@ -98,6 +150,7 @@ def install():
 def start():
     """
     Start couch, or, in the case where couch is already running, restart couch.
+
     """
     if not subprocess.call(['service', 'couchdb', 'status']):  # 'not' because 0 means 'a-okay'
         subprocess.check_call(['service', 'couchdb', 'restart'])
@@ -109,6 +162,7 @@ def start():
 def stop():
     """
     Halt couch.
+
     """
     subprocess.check_call(['service', 'couchdb', 'stop'])
 
@@ -116,15 +170,22 @@ def stop():
 @hook('db-relation-joined')
 def db_relation_joined():
     """
-    # TODO: Actually convert this into Python.
-    # TODO: figure out how to get the right values for couchdb-host and couchdb-ip.
+    Hook to run when somebody connects to us.
+
     """
-    subprocess.check_call([
-        'relation-set',
-        'host={}'.format(config('couchdb-host')),
-        'ip={}'.format(config('couchdb-ip')),
-        'port={}'.format(config('couchdb-port'))
-    ])
+
+    passwords = leader_get("passwords")
+
+    # TODO: figure out how to get the right values for couchdb-host and couchdb-ip.
+    relation_set(
+        host=config('couchdb-host'),
+        ip=config('couchdb-ip'),
+        port=config('couchdb-port'),
+        admin_pass=passwords['admin_pass'],
+        repl_pass=passwords['repl_pass']
+    )
+
+    # TODO: Figure out what this was meant to do, and convert to more purely Python equivalent.
     subprocess.check_call([
         'echo',
         '$ENSEMBLE_REMOTE_UNIT',
--- tests/01-test_config.py
+++ tests/01-test_config.py
@@ -1,36 +1,33 @@
 #!/usr/bin/env python3
 
+import mock
 import os
 import sys
 import unittest
 
+# @HACK Since "charm test" does not appear to work with the current
+# devel version of juju2, we are going to do terrible things to our
+# environment and path in order to make the tests runnable in our
+# devel dir. This hack should not make it into production code!
+if not "reactive" in os.listdir() and not "tests" in os.listdir():
+    # This is a hack, but that doesn't mean we shouldn't drop
+    # any dev running this a polite hint about why the hack is
+    # breaking, if it breaks.
+    raise Exception(
+        "This test will not run unless you start it from the root of the charm.")
+
+sys.path.append(os.getcwd())
+# END @HACK
+
+from reactive import couchdb
+
 class TestConfig(unittest.TestCase):
     """
     Test our config helper to verify that it does the right thing.
+
     """
 
-    def _temp_environ_hack(self):
-        """
-        @HACK Since "charm test" does not work with the current devel
-        version of juju2, we are going to do terrible things to our
-        environment and path in order to make the tests runnable in
-        our devel dir. This hack should not make it into production
-        code!
-        """
-        if not "reactive" in os.listdir() and not "tests" in os.listdir():
-            # This is a hack, but that doesn't mean we shouldn't drop
-            # any dev running this a polite hint about why the hack is
-            # breaking, if it breaks.
-            raise Exception(
-                "This test will not run unless you start it from the root of the charm.")
-
-        sys.path.append(os.getcwd())
-
-        self._environ_hack = True  # Flag for our tearDown.
-
     def setUp(self):
-        self._environ_hack = False
-        self._temp_environ_hack()
         self._expected_files = []  # tearDown will use this list. Any
                                    # tests that create files should
                                    # populate it.
@@ -40,36 +37,72 @@ class TestConfig(unittest.TestCase):
         for path in self._expected_files:
             os.remove(path)
 
-        # Undo our path hacks
-        if self._environ_hack:
-            sys.path.pop()
-
-    def test_config(self):
-        """Test to verify that our _set_couch_config writes files to
-        the right place.
-
-        TODO: Test backup logic by created local.ini and default.ini
-        first, and verifying that they get backed up.
-
-        TODO: improve the patch of config in the body of this routine
-        so that we get real values, and validate the the values wind
-        up in the right place.
+    @mock.patch('reactive.couchdb.leader_get')
+    @mock.patch('reactive.couchdb.leader_set')
+    def test_maybe_generate_passwords(self, mock_leader_set, mock_leader_get):
+        """
+        Basic tests for maybe_generate_passwords
 
         """
-        from reactive import couchdb as reactive_handlers  # This is what happens, Larry.
-                                                           # This is what happens when you use
-                                                           # a environ hack!
-        reactive_handlers.config = lambda *args, **kwargs: "foo"  # TODO: using mock.patch would
-                                                                  # be more polite.
+        # If we already have the passwords set, we shouldn't try to set them.
+        mock_leader_get.return_value = True
+        couchdb.maybe_generate_passwords()
+        self.assertFalse(mock_leader_set.called)
 
-        self._expected_files = [
-            '/tmp/local.ini', '/tmp/default.ini', '/tmp/juju_generated.ini'
-        ]
+        # If we don't have passwords set, we should set them.
+        mock_leader_get.return_value = False
+        couchdb.maybe_generate_passwords()
+        self.assertTrue(mock_leader_get.called)
 
-        reactive_handlers._set_couch_config(config_path="/tmp")
+        # TODO: Mock out subprocess and verify that the password dict
+        # gets populated properly.
+
+    @mock.patch('reactive.couchdb.start')
+    @mock.patch('reactive.couchdb.leader_get')
+    def test_end_admin_party(self, mock_leader_get, mock_start):
+        """
+        Cursory test for end_admin_party -- just verify that it writes
+        out a local.ini for now.
+
+        """
+        mock_leader_get.return_value = {
+            'admin_pass': 'foo',
+            'repl_pass': 'bar'
+        }
+        self._expected_files = ['/tmp/local.ini']
+        with open('/tmp/local.ini', 'w') as f:
+            f.write(';Test')
+
+        couchdb.end_admin_party(config_path='/tmp')
+
         for path in self._expected_files:
             self.assertTrue(os.path.exists(path))
             self.assertTrue(os.path.isfile(path))
 
+    @mock.patch('reactive.couchdb.config')
+    def test_write_couch_configs(self, mock_config):
+        """
+        Test to verify that our _write_couch_configs routine writes files
+        to the right place.
+
+        """
+        mock_config.return_value = 'foo'
+        
+        self._expected_files = ['/tmp/local.ini', '/tmp/default.ini']
+        for path in self._expected_files:
+            with open(path, 'w') as f:
+                f.write(';Test')
+        self._expected_files.append('/tmp/local.ini.bak')
+        self._expected_files.append('/tmp/default.ini.bak')
+
+        couchdb._write_couch_configs(config_path='/tmp')
+        for path in self._expected_files:
+            self.assertTrue(os.path.exists(path))
+            self.assertTrue(os.path.isfile(path))
+
+        # TODO: validate the contents of the config files
+            
+
 if __name__ == '__main__':
     unittest.main()
+
